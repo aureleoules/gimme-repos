b12e028a799a179d65f67cd6d98efe587eb3f2bf
unknown <aurele@oules.com>
2020-01-25 21:35:15 +0100

    basic project display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Repository from './components/Repository';
 
 function App(props) {
+
+	const [repos, setRepos] = useState([]);
+
+	useEffect(() => {
+		axios.get('/unknown').then(response => {
+			setRepos(response.data.items);
+			console.log(response.data);
+		}).catch(err => {
+			if(err) throw err;
+		});
+	}, []);
+
     return (
-		<div>
-			Hello
+		<div className="repositories">
+			<div className="container">
+				{repos.map(r => {
+					return <Repository {...r}/>
+				})}
+			</div>
 		</div>
 	)
 }
--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './styles.scss';
 
 function Repository(props) {
+
+    const defaultDescription = "No information for this project."
+
     return (
         <div className="repository">
-
+            <a target="_blank" href={props.html_url}>
+                {props.name}
+            </a>
+            <p>{props.description || defaultDescription}</p>
         </div>
     )
 }
